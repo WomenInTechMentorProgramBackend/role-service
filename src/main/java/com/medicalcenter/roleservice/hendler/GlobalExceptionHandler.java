d26fefafc0c8af7c4cf7d961711f2ce25be2c06c
taya reut <tasyareut@gmail.com>
2023-07-15 14:08:01 +0200

    Add logging to ControllerAdvice

--- src/main/java/com/medicalcenter/roleservice/hendler/GlobalExceptionHandler.java
+++ src/main/java/com/medicalcenter/roleservice/hendler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.medicalcenter.roleservice.hendler;
 import com.medicalcenter.roleservice.dto.ErrorMessage;
 import com.medicalcenter.roleservice.exception.ObjectAlreadyExistException;
 import com.medicalcenter.roleservice.exception.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +12,14 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorMessage> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        log.error("ResourceNotFoundException: {}", ex.getMessage(), ex);
+
         ErrorMessage message = new ErrorMessage(
                 HttpStatus.NOT_FOUND.value(),
                 HttpStatus.NOT_FOUND,
@@ -28,6 +32,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorMessage> handleObjectAlreadyExistException (ObjectAlreadyExistException ex, WebRequest request) {
+        log.error("ObjectAlreadyExistException: {}", ex.getMessage(), ex);
+
         ErrorMessage message = new ErrorMessage(
                 HttpStatus.CONFLICT.value(),
                 HttpStatus.CONFLICT,
